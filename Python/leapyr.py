6c64fea9e3ab288e2aebdc0925aa600722121ee3
Devikadhanalakshmi <devikadhanalakshmi@gmail.com>
2018-01-30 21:20:32 +0530

    Create leapyr.py

---
+++ Python/leapyr.py
@@ -0,0 +1,9 @@
+yr=raw_input()
+  if(yr%4==0 && yr%100!=0):
+    print leap year
+  elif(yr%100==0):
+    print not leap year
+  elif(yr%400==0):
+    print leap year
+  else:
+    print not leap year
