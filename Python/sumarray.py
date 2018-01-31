da5d58f89c9eabd7708e16ef982a86eafc984e19
Devikadhanalakshmi <devikadhanalakshmi@gmail.com>
2018-01-31 18:24:17 +0530

    Update sumarray.py

--- Python/sumarray.py
+++ Python/sumarray.py
@@ -1,9 +1,10 @@
-i=0
+i=1
 sum=0
 n=int(raw_input())
 k=int(raw_input())
-  for i in xrange(n):
-    print i
-  for i in xrange(k)
-    sum=sum+i
-      print sum
+for i in range(1,n):
+  print i
+for i in range(k):
+  sum=sum+i
+  
+print sum
