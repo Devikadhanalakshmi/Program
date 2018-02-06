aecb9445e43613c6b2d9ce82d3160f528b53d554
Devikadhanalakshmi <devikadhanalakshmi@gmail.com>
2018-02-06 13:48:30 +0530

    Create weekdays.py

---
+++ Python/weekdays.py
@@ -0,0 +1,9 @@
+weekend=["Saturday","Sunday"]
+weekdays=["Monday","Tuesday","Wednesday","Thursday","Friday"]
+n=raw_input()
+for i in range[5]:
+   if(n==weekdays[i]):
+     print no
+for i in range[2]:
+   if(n==weekend[i])
+     print yes
