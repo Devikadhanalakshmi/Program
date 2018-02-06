7853c42315808eb01cf30df71643d917319b1569
Devikadhanalakshmi <devikadhanalakshmi@gmail.com>
2018-02-06 14:03:34 +0530

    Update weekdays.py

--- Python/weekdays.py
+++ Python/weekdays.py
@@ -1,9 +1,8 @@
-weekend=["Saturday","Sunday"]
-weekdays=["Monday","Tuesday","Wednesday","Thursday","Friday"]
-n=raw_input()
-for i in range[5]:
-   if(n==weekdays[i]):
-     print no
-for i in range[2]:
-   if(n==weekend[i])
-     print yes
+list1=['monday','tuesday','wednesday','thursday','friday']
+list2=['sunday','saturday']
+day=input()
+if day in list1:
+       print("no")
+else if day in list2:
+       print("yes")
+
